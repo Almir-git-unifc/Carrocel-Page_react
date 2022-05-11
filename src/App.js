1f119b22cb00e47f4422f512c7bed1fb4553a6c0
Almir-git-unifc <1341215@sga.pucminas.br>
2022-05-11 18:29:53 -0500

    feat(json): v02.01.000 - react color text and bag values

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ function App() {
   return (
 
     <div className="container">
-      <div className="logo">
-        <img src="/static/images/super-shoes.png" alt="Super Shoes Logo" />
-      </div>
+        <div className="logo">
+              <img src="/static/images/super-shoes.png" alt="Super Shoes Logo" />
+        </div>
 
-      <div className="carousel">
-        <div className="item">
-          
-          <div className="image">
-            <img src="https://imgcentauro-a.akamaihd.net/230x230/94313731.jpg" alt="Shoe" />
-          </div>
+        <div className="carousel">
+             <div className="item">
 
-          <div className="info">
-            <span className="name">Super Shoe 1</span>
-            <span className="oldPrice">US$ 299,00</span>
-            <span className="price">US$ 199,00</span>
-          </div>
+                 <div className="image">
+                       <img src="https://imgcentauro-a.akamaihd.net/230x230/94313731.jpg" alt="Shoe" />
+                 </div>
+
+                 <div className="info">
+                        <span className="name">Super Shoe 1</span>
+                        <span className="oldPrice">US$ 299,00</span>
+                        <span className="price">US$ 199,00</span>
+                 </div>
 
 
         </div>
